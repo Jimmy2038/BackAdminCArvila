321393f744ffe43dff24238c5300ac86ad7dbafc
Jimmy2038 <jimmyrandriamiarimbola@gmail.com>
2024-01-27 20:17:26 +0100

    add new jar3

--- src/main/java/com/example/Fiaraamidy/config/WebConfig.java
+++ src/main/java/com/example/Fiaraamidy/config/WebConfig.java
@@ -22,9 +22,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://aromatic-fork-production.up.railway.app");
-        config.addAllowedOrigin("https://tiny-smakager-838b3d.netlify.app");
-        config.setAllowedHeaders(Arrays.asList(
+      config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
                 HttpHeaders.ACCEPT
